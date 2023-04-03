7d696fb95ff8c4bbf281c7631c41862128d5e9c9
gabriel andrade <tisco09090@gmail.com>
2023-04-03 08:35:39 -0400

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+            int i = 1015202530;
+        System.out.println("VARIÁVEL PRIMITVA");
+        System.out.println(i);
+            int Integer = i;
+        System.out.println("VARIÁVEL WRAPPER");
+        System.out.println(Integer);
+
+
+
+    }
+}
